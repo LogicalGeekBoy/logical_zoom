f9ee0373fdfdfa880e6d2bc12f228f7c645be4e3
LogicalGeekBoy <logicalgeekboy@gmail.com>
2024-10-22 22:29:12 +0100

    WIP Bump 1.21.2 - Need to remap getFov method

---
+++ remappedSrc/com/logicalgeekboy/logical_zoom/LogicalZoom.java
@@ -0,0 +1,82 @@
+package com.logicalgeekboy.logical_zoom;
+
+import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
+import net.minecraft.client.util.InputUtil;
+import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.option.KeyBinding;
+
+import org.lwjgl.glfw.GLFW;
+
+public class LogicalZoom implements ClientModInitializer {
+
+    private static boolean currentlyZoomed;
+    private static KeyBinding keyBinding;
+    private static boolean originalSmoothCameraEnabled;
+    private static final MinecraftClient mc = MinecraftClient.getInstance();
+
+    public static final double zoomLevel = 0.23;
+
+    @Override
+    public void onInitializeClient() {
+        keyBinding = new KeyBinding("key.logical_zoom.zoom", InputUtil.Type.KEYSYM, GLFW.GLFW_KEY_C, "category.logical_zoom.zoom");
+
+        currentlyZoomed = false;
+        originalSmoothCameraEnabled = false;
+
+        KeyBindingHelper.registerKeyBinding(keyBinding);
+    }
+
+    public static boolean isZooming() {
+        return keyBinding.isPressed();
+    }
+
+    public static void manageSmoothCamera() {
+        if (zoomStarting()) {
+            zoomStarted();
+            enableSmoothCamera();
+        }
+
+        if (zoomStopping()) {
+            zoomStopped();
+            resetSmoothCamera();
+        }
+    }
+
+    private static boolean isSmoothCamera() {
+        return mc.options.smoothCameraEnabled;
+    }
+
+    private static void enableSmoothCamera() {
+        mc.options.smoothCameraEnabled = true;
+    }
+
+    private static void disableSmoothCamera() {
+        mc.options.smoothCameraEnabled = false;
+    }
+
+    private static boolean zoomStarting() {
+        return isZooming() && !currentlyZoomed;
+    }
+
+    private static boolean zoomStopping() {
+        return !isZooming() && currentlyZoomed;
+    }
+
+    private static void zoomStarted() {
+        originalSmoothCameraEnabled = isSmoothCamera();
+        currentlyZoomed = true;
+    }
+
+    private static void zoomStopped() {
+        currentlyZoomed = false;
+    }
+
+    private static void resetSmoothCamera() {
+        if (originalSmoothCameraEnabled) {
+            enableSmoothCamera();
+        } else {
+            disableSmoothCamera();
+        }
+    }
+}
